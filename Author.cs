9e8986decd646d26f617878feaaf459b1abe9282
anichalakova <anichalakova@users.noreply.github.com>
2015-01-06 00:59:19 +0200

    Added UnitTests folder and Author.cs

---
+++ Author.cs
@@ -0,0 +1,18 @@
+﻿namespace BalloonsPop
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct |
+    AttributeTargets.Class | AttributeTargets.Interface,
+    AllowMultiple = true)]
+
+    public class AuthorAttribute : System.Attribute
+    {
+        public string Name { get; private set; }
+
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
