423868def3de26741789687ea3ccb3e10baad640
anichalakova <anichalakova@users.noreply.github.com>
2015-01-07 00:42:27 +0200

    Added UnitTests folder

---
+++ Refactored Project/BaloonsPop_UnitTests/Balloons_Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace Baloons_UnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BalloonsPop;
+
+
+    // All classes and all methods should be tested for correct behavior and for exceptions! 
+    // Test largest and smallest possible values!
+
+    [TestClass]
+    public class Baloons_Tests
+    {
+        [TestMethod]
+        public void FillWithRandomBalloonsShouldReturnFilledArray()
+        {
+            Balloons.FillWithRandomBalloons();
+
+            Assert.IsNotNull(Balloons.cell[Balloons.Rows-1,Balloons.Columns-1]);
+        }
+    }
+}
